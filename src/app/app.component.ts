1cd9da93d32c38432cf883c7bcaf39349c25e3cc
vcsprg6 <vcs-prg6@supportgov.in>
2025-11-13 15:05:14 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
+import { BnNgIdleService } from 'bn-ng-idle';
+
+import { FormControl } from '@angular/forms';
+// import { OverlayContainer } from '@angular/cdk/overlay';
+
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'documentManagmentSystem';
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+  getFileUpload() {
+    alert('nishant')
+  }
 }
